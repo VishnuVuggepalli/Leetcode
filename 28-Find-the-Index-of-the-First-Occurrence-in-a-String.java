88041ab6fcf565137d7de53bffd440244fe4a7a8
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2024-11-10 21:53:43 -0500

    [28] [Time Beats: 100.00%] [Memory Beats: 64.57%] - LeetPush

--- 28-Find-the-Index-of-the-First-Occurrence-in-a-String.java
+++ 28-Find-the-Index-of-the-First-Occurrence-in-a-String.java
@@ -1,5 +1,11 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        for(int first = 0 ; first <= haystack.length()- needle.length(); first ++){
+                    if(haystack.substring(first, first + needle.length()).equals(needle)){
+                        return first;
+                    }
+        }
+        return -1;
+        //return haystack.indexOf(needle)
     }
 }
