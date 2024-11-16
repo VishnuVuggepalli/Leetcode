f3162e5f8c3d3c5a5958978fbf72692289677b4f
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2024-11-16 10:57:53 -0500

    [3] [Time Beats: 39.14%] [Memory Beats: 26.38%] - LeetPush

--- 3-Longest-Substring-Without-Repeating-Characters.java
+++ 3-Longest-Substring-Without-Repeating-Characters.java
@@ -1,23 +1,18 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
-        for(int subLeft =0; subLeft + maxLength < s.length(); subLeft ++)
+        int subLeft = 0;
+        Set<Character> unqChar = new HashSet<Character>();
+        for(int subRight =0; subRight < s.length(); subRight ++)
         {
-            int tempLen =0, subRight =0;
-            Set<Character> unqChar = new HashSet<Character>();
-            //System.out.println(\in loop 1  \ + s.charAt(subLeft));
-            for(subRight = subLeft; subLeft + (subRight - subLeft) < s.length(); subRight++){
-                //System.out.println(\in loop 2  \ + s.charAt(subRight));
-                if(unqChar.contains(s.charAt(subRight))){
-                    //System.out.println(\in contains \ + s.charAt(subRight));
-                    break;
+            int tempLen =0;
+            while(unqChar.contains(s.charAt(subRight))){
+                    unqChar.remove(s.charAt(subLeft));
+                    subLeft++;
                 }
                 unqChar.add(s.charAt(subRight));
                 tempLen++;
-            }
-            if(maxLength < subRight - subLeft){
-                maxLength = subRight - subLeft;
-            }
+            maxLength = Math.max(maxLength, subRight-subLeft+1);
         }
         return maxLength;
     }
