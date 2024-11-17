74cee5a4f3d5644be0c0f9c1a3cdac3030117249
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2024-11-17 16:42:52 -0500

    [682] [Time Beats: 5.69%] [Memory Beats: 5.11%] - LeetPush

---
+++ 682-Baseball-Game.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int calPoints(String[] operations) {
+      int sum =0;
+      Stack<String> stack = new Stack<>();
+      for(int op =0 ; op < operations.length ; op++){
+        switch(operations[op]){
+            case \+\: 
+                int blockSum =0;
+                String temp = \\;
+                temp = stack.pop();
+                blockSum = Integer.parseInt(temp) + Integer.parseInt(stack.peek());
+                stack.push(\\ +temp);
+                stack.push(\\ +blockSum);
+                break;
+            case \C\: 
+                stack.pop();
+                break;
+            case \D\: 
+                int doubleScore =0;
+                doubleScore = Integer.parseInt(stack.peek());
+                stack.push(\\ + (doubleScore * 2));
+                break;
+            default: 
+                stack.push(operations[op]);
+                break;
+        }
+        System.out.println(stack);
+      }
+      while(!stack.isEmpty()){
+        sum += Integer.parseInt(stack.pop());
+      }
+      return sum;
+    }
+}
