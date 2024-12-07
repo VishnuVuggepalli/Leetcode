a620523940eea82248d73986695f389768159df1
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2024-12-07 12:17:19 -0500

    [128] [Time Beats: 44.95%] [Memory Beats: 5.03%] - LeetPush

---
+++ 128-Longest-Consecutive-Sequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums= set(nums)
+        val_count_map = {}
+        max_val = 0
+        for num in nums:
+            low = val_count_map.get(num-1,0)
+            high = val_count_map.get(num+1,0)
+            val = low+ high +1
+            val_count_map[num - low] = val
+            val_count_map[num + high] = val
+            max_val = max(val, max_val)
+        return max_val
