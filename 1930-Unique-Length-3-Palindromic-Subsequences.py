3ffe51901c893a3f9b03e0e62a7c1757097eb7f9
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2024-12-14 13:36:35 -0500

    [1930] [Time Beats: 54.35%] [Memory Beats: 18.69%] - LeetPush

---
+++ 1930-Unique-Length-3-Palindromic-Subsequences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        visited = []
+        palindrome_count =0
+        for left in range(len(s)):
+            right = len(s)-1
+            if s[left] not in visited:
+                visited.append(s[left])
+                while right > left and s[right] != s[left]:
+                    print(right)
+                    right -= 1
+                palindrome_count += len(set(s[left+1 : right]))
+        return palindrome_count
