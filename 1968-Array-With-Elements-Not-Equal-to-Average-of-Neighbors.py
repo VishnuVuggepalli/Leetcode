0295ddb8c0b674cbec390c55a704760e9e6976c5
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2024-12-22 12:40:03 -0500

    [1968] [Time Beats: 90.13%] [Memory Beats: 11.69%] - LeetPush

---
+++ 1968-Array-With-Elements-Not-Equal-to-Average-of-Neighbors.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        '''
+        break sortedness
+        if in order , break the order , replace mid with larger
+        '''
+        for i in range(1,len(nums)-1):
+            if (nums[i -1] < nums[i] and nums[i] < nums[i +1]) or (nums[i-1] > nums[i] and nums[i] > nums[i+1]):
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+
+        return nums
