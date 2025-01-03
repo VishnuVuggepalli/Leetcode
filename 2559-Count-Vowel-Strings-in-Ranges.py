a118448b081da84de801822a1eab629f737ff9a5
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-01-02 22:23:34 -0500

    [2559] [Time Beats: 93.21%] [Memory Beats: 47.98%] - LeetPush

--- 2559-Count-Vowel-Strings-in-Ranges.py
+++ 2559-Count-Vowel-Strings-in-Ranges.py
@@ -1,16 +1,18 @@
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        n = len(words)
-        Prefix = [0] * (n + 1)
-        vowels = {'a', 'e', 'i', 'o', 'u'}
-
-        for i in range(n):
-            Prefix[i + 1] = Prefix[i]
-            if words[i][0] in vowels and words[i][-1] in vowels:
-                Prefix[i + 1] += 1
-
-        ANS = []
-        for query in queries:
-            ANS.append(Prefix[query[1] + 1] - Prefix[query[0]])
-
-        return ANS
+        
+        res =[]
+        pref = []
+        vowels = ("a","e","i","o","u")
+        cnt  = 0
+        for cnr in words:
+            if cnr[0] in vowels and cnr[-1] in vowels:
+                cnt += 1
+            pref.append(cnt)
+        for i,j in queries:
+            if i > 0 :
+                diff = pref[j] - pref[i-1]
+            else:
+                diff = pref[j] - 0
+            res.append(diff)
+        return res
