3ac681963577a798955db204ac502f8d12da4244
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-01-04 10:19:42 -0500

    [1669] [Time Beats: 31.59%] [Memory Beats: 8.63%] - LeetPush

--- 1669-Merge-In-Between-Linked-Lists.py
+++ 1669-Merge-In-Between-Linked-Lists.py
@@ -5,21 +5,34 @@
 #         self.next = next
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        count = 0
-        dummy = ListNode(0,list1)
-        prev = dummy
+        prev_a , post_b = list1, list1
+        for _ in range(a-1):
+            prev_a = prev_a.next
+        for _ in range(b+1):
+            post_b = post_b.next
+        
+        prev_a.next = list2
+        while prev_a.next:
+            prev_a = prev_a.next
+        
+        prev_a.next = post_b
 
-        while prev:
-            if count == a:
-                temp = prev.next
-                prev.next = list2
-                while prev.next:
-                    prev = prev.next
-                while count <= b:
-                    count += 1
-                    temp = temp.next
-                prev.next = temp
-            else:
-                count += 1
-                prev = prev.next
-        return dummy.next
+        return list1
+        # count = 0
+        # dummy = ListNode(0,list1)
+        # prev = dummy
+
+        # while prev:
+        #     if count == a:
+        #         temp = prev.next
+        #         prev.next = list2
+        #         while prev.next:
+        #             prev = prev.next
+        #         while count <= b:
+        #             count += 1
+        #             temp = temp.next
+        #         prev.next = temp
+        #     else:
+        #         count += 1
+        #         prev = prev.next
+        # return dummy.next
