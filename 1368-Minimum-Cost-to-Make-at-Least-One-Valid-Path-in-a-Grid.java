5a5bffc7476bd330327f6db2b4f85755154bc9af
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-01-19 10:18:20 -0500

    [1368] [Time Beats: 81.12%] [Memory Beats: 66.08%] - LeetPush

--- 1368-Minimum-Cost-to-Make-at-Least-One-Valid-Path-in-a-Grid.java
+++ 1368-Minimum-Cost-to-Make-at-Least-One-Valid-Path-in-a-Grid.java
@@ -1,42 +1,39 @@
 class Solution {
     public int minCost(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-
-        int[][] minCost=new int[m][n];
-        for(int i=0; i<m; i++){
+        int M = grid.length;
+        int N = grid[0].length;
+        int[][] minCost = new int[M][N];
+        for(int i = 0; i< M; i++){
             Arrays.fill(minCost[i], Integer.MAX_VALUE);
         }
 
-        Deque<int[]> dque=new ArrayDeque<>();
-        dque.offerFirst(new int[]{0,0});
-        minCost[0][0]=0;
-        
-        int[][] direction={ {0,1}, {0,-1}, {1,0}, {-1,0} };
+        Deque<int[]> Q = new ArrayDeque<>();
+        Q.offerFirst(new int[]{0,0});
+        minCost[0][0] = 0;
 
-        while(!dque.isEmpty()){
-            int[] arr=dque.pollFirst();
-            int r=arr[0];
-            int c=arr[1];
+        int[][] direction = {{0,1},{0,-1},{1,0},{-1,0}};
+        while(!Q.isEmpty()){
+            int[] arr = Q.pollFirst();
+            int r = arr[0];
+            int c = arr[1];
 
-            //Visited adjacent cells
-            for(int i=0; i<4; i++){
-                int nr=r+direction[i][0];
-                int nc=c+direction[i][1];
-                int cost=(grid[r][c]!=(i+1))?1:0;
+            for(int i = 0; i < 4; i++){
+                int nr = r + direction[i][0];
+                int nc = c + direction[i][1];
+                int cost = (grid[r][c] != (i+1)) ? 1 : 0;
 
-                if( nr>=0 && nr<m && nc>=0 && nc<n && minCost[r][c]+cost<minCost[nr][nc]){
-                    minCost[nr][nc]=minCost[r][c]+cost;
+                if(nr >= 0 && nr< M && nc < N && nc >= 0 && minCost[r][c] + cost < minCost[nr][nc]){
+                    minCost[nr][nc] = minCost[r][c] + cost;
 
-                    if(cost==1){
-                        dque.offerLast(new int[]{nr,nc});
-                    }else{
-                        dque.offerFirst(new int[]{nr,nc});
-                    }                   
+                    if(cost == 1){
+                        Q.offerLast(new int[]{nr,nc});
+                    }
+                    else{
+                        Q.offerFirst(new int[]{nr,nc});
+                    }
                 }
             }
         }
-
-        return minCost[m-1][n-1];
+        return minCost[M-1][N-1];
     }
 }
