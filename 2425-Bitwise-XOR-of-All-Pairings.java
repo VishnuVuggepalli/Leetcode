20e4c2f2163f7b8e91b66ab82ce01b6976f9b397
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-01-20 13:37:30 -0500

    [2425] [Time Beats: 99.97%] [Memory Beats: 11.95%] - LeetPush

--- 2425-Bitwise-XOR-of-All-Pairings.java
+++ 2425-Bitwise-XOR-of-All-Pairings.java
@@ -1,10 +1,29 @@
 class Solution {
-    public int xorAllNums(int[] A, int[] B) {
-        int x = 0, y = 0;
-        for (int a: A)
-            x ^= a;
-        for (int b: B)
-            y ^= b;
-        return (A.length % 2 * y) ^ (B.length % 2 * x);
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int x = 0;
+        int M = nums1.length, N = nums2.length;
+        if ((M & 1) == 0 && (N & 1) == 0) return x;
+        if ((M & 1) == 1 && (N & 1) == 1){
+            for(int i = 0; i < M; i++){
+                x ^= nums1[i];
+            }
+    
+            for(int i = 0; i < N; i++){
+                x ^= nums2[i];
+            }
+
+            return x;
+        }
+        if ( (M & 1) == 0){
+            for(int i = 0; i < M; i++){
+                x ^= nums1[i];
+            }
+            return x;
+        }
+        for(int i = 0; i < N; i++){
+            x ^= nums2[i];
+        }
+
+        return x;
     }
 }
