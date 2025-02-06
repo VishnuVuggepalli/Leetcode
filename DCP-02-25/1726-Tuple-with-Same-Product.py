f92b02a4cda43246fac84dca6fe323d2ce66f147
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-02-06 10:20:58 -0500

    [1726] [Time Beats: 90.52%] [Memory Beats: 65.40%] - LeetPush

--- DCP-02-25/1726-Tuple-with-Same-Product.py
+++ DCP-02-25/1726-Tuple-with-Same-Product.py
@@ -1,21 +1,15 @@
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
         prod_count = {}
-        max_set = set()
+        tuples = 0
         N = len(nums)
         for i in range(N-1):
             for j in range(i+1,N):
                 cur_prod = nums[i] * nums[j]
-                if cur_prod not in prod_count:
-                    prod_count[cur_prod] = 0
-                prod_count[cur_prod] += 1
-                if prod_count[cur_prod] > 1:
-                    max_set.add(cur_prod)
-        #         print(nums[i], nums[j])
-        # print(prod_count)
-        tuples = 0
-        for i in max_set:
-            tuples += (((prod_count[i] * (prod_count[i] -1)) // 2) * 8)
-        
-
+                if cur_prod in prod_count:
+                    tuples += 8 * prod_count[cur_prod]
+                    prod_count[cur_prod] += 1
+                else:
+                    prod_count[cur_prod] = 1
+          
         return tuples
