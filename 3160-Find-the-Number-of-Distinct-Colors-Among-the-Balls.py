7b98ef590e6aab4b35b502029f70d8a2fc63c0e5
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-02-08 11:05:33 -0500

    [3160] [Time Beats: 87.44%] [Memory Beats: 81.16%] - LeetPush

--- 3160-Find-the-Number-of-Distinct-Colors-Among-the-Balls.py
+++ 3160-Find-the-Number-of-Distinct-Colors-Among-the-Balls.py
@@ -1,18 +1,19 @@
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        n=len(queries)
-        ans=[0]*n
-        mp={}
-        color=defaultdict(int)
-        i=0
-        for x, c in queries:
-            if x in mp:
-                c0=mp[x]
-                color[c0]-=1
-                if color[c0]==0:
-                    color.pop(c0)
-            mp[x]=c
-            color[c]+=1
-            ans[i]=len(color)
-            i+=1
-        return ans
+        colors = defaultdict(int)
+        visited = {}
+        res = []
+        for i, color in queries:
+            if i not in visited:
+                visited[i] = color
+            else:
+                if colors[visited[i]] == 1:
+                    del colors[visited[i]]
+                else:
+                    colors[visited[i]] -= 1
+                visited[i] = color
+            colors[color] += 1
+            res.append(len(colors))
+        
+        return res
+            
