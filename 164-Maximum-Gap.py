12897ad1eae40ffd8177230e80f7f7869bb14f04
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-02-13 10:22:08 -0500

    [164] [Time Beats: 85.76%] [Memory Beats: 23.56%] - LeetPush

--- 164-Maximum-Gap.py
+++ 164-Maximum-Gap.py
@@ -1,9 +1,16 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
-        max_diff = 0
-        heapq.heapify(nums)
-
-        while len(nums) > 1:
-            cur = heapq.heappop(nums)
-            max_diff = max(max_diff, nums[0] - cur)
+        if len(nums) <= 1:
+            return 0
+        nums = sorted(set(nums))
+        max_diff =0
+        for i in range(1, len(nums)):
+            max_diff = max(nums[i] - nums[i-1], max_diff)
         return max_diff
+        # max_diff = 0
+        # heapq.heapify(nums)
+
+        # while len(nums) > 1:
+        #     cur = heapq.heappop(nums)
+        #     max_diff = max(max_diff, nums[0] - cur)
+        # return max_diff
