99a73a331981eb1fea9d922804d309e392104ac0
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-02-12 20:19:39 -0500

    [448] [Time Beats: 19.84%] [Memory Beats: 89.55%] - LeetPush

---
+++ 448-Find-All-Numbers-Disappeared-in-an-Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+
+        for n in nums:
+            i = abs(n) - 1
+            nums[i] = -1 * abs(nums[i])
+        
+        res = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res.append(i+1)
+        return res
