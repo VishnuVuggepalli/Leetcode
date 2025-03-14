e3618faa6c8f4a2266df566da2595c85149209e3
Vuggepalli Vishnu <55404692+VishnuVuggepalli@users.noreply.github.com>
2025-03-14 15:33:53 -0400

    [2226] [Time Beats: 90.99%] [Memory Beats: 91.40%] - LeetPush

---
+++ DCP-03-25/Maximum-Candies-Allocated-to-K-Children.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        int N = candies.length;
+        int maxCandies = 0;
+        int upperBound = 0;
+        for (int c: candies){
+            upperBound = Math.max(upperBound, c);
+        }
+        int lowerBound  = 1;
+        while(lowerBound <= upperBound){
+            int mid = (lowerBound + upperBound ) / 2;
+            long sum = 0;
+            for(int i = 0; i < N; i++){
+                sum += candies[i] / mid;
+            }
+            // System.out.println(lowerBound + " " +  upperBound + " " + sum);
+            if (sum >= k){
+                maxCandies = mid;
+                lowerBound = mid + 1;
+            }
+            else{
+                upperBound = mid - 1; 
+            }
+        }
+        return maxCandies;
+    }
+}
